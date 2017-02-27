6dedb6bbb8bdff6e68cfb5b8fbef059bdb6fcb9c
soe <sopykt09@gmail.com>
2017-02-27 23:51:29 +0630

    toLowerCase

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
+    switch (messageText.toLowerCase()) {
       case 'image':
         sendImageMessage(senderID);
         break;
