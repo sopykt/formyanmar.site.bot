3c348cfbaf36902ce0181a55c68622b36d303ac9
sopykt <sopykt09@gmail.com>
2017-03-10 23:18:09 +0630

    webview in gernric test

--- index.js
+++ index.js
@@ -275,6 +275,10 @@ function receivedMessage(event) {
       case 'generic':
         sendGenericMessage(senderID);
         break;
+        
+	  case 'test generic':
+		sendTestGenericMessage(senderID);
+		break;
 
       case 'receipt':
         sendReceiptMessage(senderID);
@@ -709,6 +713,58 @@ function sendGenericMessage(recipientId) {
               type: "web_url",
               url: "https://www.oculus.com/en-us/rift/",
               title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: SERVER_URL + "/assets/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+/*
+ * test generic message for webview
+ *
+ */
+function sendTestGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: SERVER_URL + "/assets/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: SERVER_URL + "/assets/form.html",
+              title: "Buy Now",
+              webview_height_ratio:"compact"
             }, {
               type: "postback",
               title: "Call Postback",
