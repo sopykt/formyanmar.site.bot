659068cc5f69e2b91d93f378ff51c3d148aa9f3a
sopykt <sopykt09@gmail.com>
2017-03-18 12:05:57 +0630

    blood tubes generic qr added

--- index.js
+++ index.js
@@ -869,7 +869,24 @@ function sendBloodTubeGenericMessage(recipientId) {
             }],
           }]
         }
-      }
+      },
+      quick_replies: [
+        {
+          "content_type":"text",
+          "title":"Action",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+        },
+        {
+          "content_type":"text",
+          "title":"Comedy",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+        },
+        {
+          "content_type":"text",
+          "title":"Drama",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+        }
+      ]
     }
   };  
 
