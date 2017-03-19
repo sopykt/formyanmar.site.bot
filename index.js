3f74d2c8460f7bbdf6bc167fa889436e63fbea50
sopykt <sopykt09@gmail.com>
2017-03-19 11:19:30 +0630

    persistent menu set

--- index.js
+++ index.js
@@ -1332,6 +1332,65 @@ function callSendAPI(messageData) {
   });  
 }
 
+//post persistent menu
+function setPersistentMenu()	{
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+		qs: {access_token: token},
+		method: 'POST',
+		jason: {
+  persistent_menu:[
+    {
+      locale:"default",
+      composer_input_disabled:true,
+      call_to_actions:[
+        {
+          title:"My Account",
+          type:"nested",
+          call_to_actions:[
+            {
+              title:"Pay Bill",
+              type:"postback",
+              payload:"PAYBILL_PAYLOAD"
+            },
+            {
+              title:"History",
+              type:"postback",
+              payload:"HISTORY_PAYLOAD"
+            },
+            {
+              title:"Contact Info",
+              type:"postback",
+              payload:"CONTACT_INFO_PAYLOAD"
+            }
+          ]
+        },
+        {
+          type:"web_url",
+          title:"Latest News",
+          url:"http://petershats.parseapp.com/hat-news",
+          webview_height_ratio:"full"
+        }
+      ]
+    },
+    {
+      locale:"zh_CN",
+      composer_input_disabled:false
+    }
+  ]
+}
+		},	function (error, response, body)	{
+			if (!error && response.statusCode == 200)	{
+				console.log("persistent menu successfully set");
+				} else {
+					console.error("error setting persistent menu");
+					}
+			}
+	);
+	}
+	
+	setPersistentMenu();
+
 // spin spin sugar
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
